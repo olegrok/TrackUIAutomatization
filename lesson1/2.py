ebe8f88499f7a443bc862dff3be07467a6f26e25
Babin Oleg <babinoleg@mail.ru>
2018-03-06 16:54:39 +0300

    2d solution fix

--- lesson1/2.py
+++ lesson1/2.py
@@ -15,6 +15,6 @@ HalfMax = 100
 array = [2 * random.randint(0, HalfMax) for i in range(N)]
 print("Исходный массив:", array)
 for i in range(N):
-    if i % 2 == 0 and i != 0:
+    if i % 2 != 0:
         array[i] *= array[i - 1]
 print("Итоговый массив:", list(array))
